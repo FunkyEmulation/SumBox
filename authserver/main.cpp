374dc5fe1852e5d8cb7ad3f4d2130e23d5d02171
Kevin <kevainxxx@gmail.com>
2012-12-09 17:59:53 +0200

    Fermeture de l'application propre en interceptant l'événement SIGINT (Ctrl+c).
    Signed-off-by: Kevin <kevainxxx@gmail.com>

--- authserver/main.cpp
+++ authserver/main.cpp
@@ -1,10 +1,21 @@
 #include <QCoreApplication>
 #include <QTime>
 #include <iostream>
+#include <csignal>
 #include "../shared/define.h"
 #include "authserver.h"
 
 using namespace std;
+
+AuthServer authserver;
+
+void exit(int /*s*/)
+{
+    cout << "Stopping SumBox::Authserver..." << endl;
+    authserver.Stop();
+    cout << "SumBox::Authserver stopped. Exiting..." << endl;
+    QCoreApplication::exit();
+}
 
 int main(int argc, char *argv[])
 {
@@ -14,17 +25,17 @@ int main(int argc, char *argv[])
     t.start();
     cout << "Starting SumBox::Authserver..." << endl;
 
-    AuthServer authServer;
-    if(!authServer.Start(QHostAddress("127.0.0.1"), 443))
+    if(!authserver.Start(QHostAddress("127.0.0.1"), 443))
     {
-        cout << authServer.GetErrorString().toAscii().data() << endl;
+        cout << authserver.GetErrorString().toAscii().data() << endl;
         return 0;
     }
     else
         cout << "AuthServer started : waiting for connections" << endl;
 
     cout << "Press ctrl + c to quit." << endl;
-    cout << "Started in " << QString::number(t.elapsed() / IN_MILLISECONDS).toAscii().data() << " sec." << endl;
+    cout << "SumBox::Authserver started in " << QString::number(t.elapsed() / IN_MILLISECONDS).toAscii().data() << " sec." << endl;
 
+    signal(SIGINT, &exit);
     return a.exec();
 }
