20837d315ac71639a642e76899ad779d137d5877
Kevin <kevainxxx@gmail.com>
2013-03-21 09:39:46 +0200

    Travail sur les commandes consoles, ne fonctionne toujours pas (il est désactivé).
    Signed-off-by: Kevin <kevainxxx@gmail.com>

--- worldserver/game/chat/commandline.h
+++ worldserver/game/chat/commandline.h
@@ -4,6 +4,7 @@
 #include <QtCore>
 #include <iostream>
 #include "chat.h"
+#include "../world/world.h"
 
 using namespace std;
 
@@ -15,15 +16,50 @@ public:
 
     virtual void run()
     {
+        printf("Sumbox>");
+
         string input;
         char args[256];
-        while (cin >> input)
+        while (World::Instance()->IsRunning())
         {
-            cin.get(args, 256);
-            QString command = QString::fromStdString(input);
-            command += QString::fromStdString(args);
+            fflush(stdout);
 
-            Chat::Instance()->ParseCommand(command);
+            char* command_str;
+            char commandbuff[256];
+
+            command_str = fgets(commandbuff, sizeof(commandbuff), stdin);
+
+            if (command_str != NULL)
+            {
+                for (int x=0; command_str[x]; ++x)
+                    if (command_str[x] == '\r' || command_str[x] == '\n')
+                    {
+                        command_str[x] = 0;
+                        break;
+                    }
+
+                if (!*command_str)
+                {
+                    printf("Sumbox>");
+                    continue;
+                }
+
+
+                QString command = QString::fromStdString(input);
+
+                if (command.isEmpty())
+                {
+                    printf("Sumbox>");
+                    continue;
+                }
+
+                fflush(stdout);
+                Chat::Instance()->ParseCommand(command);
+            }
+            else if (feof(stdin))
+            {
+                World::Close();
+            }
         }
     }
 };
