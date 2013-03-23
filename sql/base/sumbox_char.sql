756a5b0656f544d0f5f38d9872594fee963ec9cb
Kevin <kevainxxx@gmail.com>
2013-03-23 23:40:12 +0200

    Ajout du fichier SQL pour la table "character" (structure ultra basique).
    Signed-off-by: Kevin <kevainxxx@gmail.com>

---
+++ sql/base/sumbox_char.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Client: localhost
+-- Généré le: Sam 23 Mars 2013 à 21:39
+-- Version du serveur: 5.5.24-log
+-- Version de PHP: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `sumbox_char`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `character`
+--
+
+CREATE TABLE IF NOT EXISTS `character` (
+  `guid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `account` int(10) unsigned NOT NULL,
+  `name` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `class` tinyint(3) unsigned NOT NULL,
+  `gender` tinyint(3) unsigned NOT NULL,
+  `level` smallint(5) unsigned NOT NULL,
+  `gfx_id` smallint(5) unsigned NOT NULL,
+  `color_1` int(11) NOT NULL,
+  `color_2` int(11) NOT NULL,
+  `color_3` int(11) NOT NULL,
+  PRIMARY KEY (`guid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
